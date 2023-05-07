4a423bf351ce758f2b199cbce92ced2669f6e9c7
文享旭 <846212939@qq.com>
2023-05-07 14:03:06 +0800

    feat:新增Bean单例管理接口

---
+++ src/main/java/me/xu/spring/beans/DefaultSingletonBeanRegistry.java
@@ -0,0 +1,56 @@
+package me.xu.spring.beans;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * Description Bean单例管理具体实现
+ * Date 2023/5/7 11:31
+ * Version 1.0.1
+ *
+ * @author Wen
+ */
+public class DefaultSingletonBeanRegistry implements SingletonBeanRegistry {
+
+    /**
+     * 存放所有Bean的名称
+     */
+    protected List<String> beanNames = new ArrayList<>();
+
+    /**
+     * Bean单例Map
+     */
+    protected Map<String, Object> singletons = new ConcurrentHashMap<>();
+
+    @Override
+    public void registerSingleton(String beanName, Object singletonObject) {
+        synchronized (singletons) {
+            singletons.put(beanName, singletonObject);
+            beanNames.add(beanName);
+        }
+    }
+
+    protected void removeSingleton(String beanName) {
+        synchronized (singletons) {
+            beanNames.remove(beanName);
+            singletons.remove(beanName);
+        }
+    }
+
+    @Override
+    public Object getSingleton(String beanName) {
+        return singletons.get(beanName);
+    }
+
+    @Override
+    public boolean containsSingleton(String beanName) {
+        return singletons.containsKey(beanName);
+    }
+
+    @Override
+    public String[] getSingletonNames() {
+        return (String[]) this.beanNames.toArray();
+    }
+}
---
+++ src/main/java/me/xu/spring/beans/SingletonBeanRegistry.java
@@ -0,0 +1,38 @@
+package me.xu.spring.beans;
+
+/**
+ * Description Bean单例管理接口
+ * Date 2023/5/7 11:18
+ * Version 1.0.1
+ *
+ * @author Wen
+ */
+public interface SingletonBeanRegistry {
+
+    /**
+     * 注册单例
+     * @param beanName Bean id
+     * @param singletonObject Bean对象
+     */
+    void registerSingleton(String beanName, Object singletonObject);
+
+    /**
+     * 获取单例
+     * @param beanName Bean id
+     * @return Bean对象
+     */
+    Object getSingleton(String beanName);
+
+    /**
+     * 是否包含某个单例Bean
+     * @param beanName Bean id
+     * @return
+     */
+    boolean containsSingleton(String beanName);
+
+    /**
+     * 获取所有单例名称
+     * @return
+     */
+    String[] getSingletonNames();
+}
