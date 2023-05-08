de714c01ffd12699f72da618b5c1dc06686c9668
文享旭 <846212939@qq.com>
2023-05-08 15:03:19 +0800

    feat:新增解析属性和构造器动作

--- src/main/java/me/xu/spring/beans/XmlBeanDefinitionReader.java
+++ src/main/java/me/xu/spring/beans/XmlBeanDefinitionReader.java
@@ -3,6 +3,8 @@ package me.xu.spring.beans;
 import me.xu.spring.core.Resource;
 import org.dom4j.Element;
 
+import java.util.List;
+
 /**
  * Description xml读取后解析为BeanDefinition
  * Date 2023/5/5 10:08
@@ -31,6 +33,34 @@ public class XmlBeanDefinitionReader {
             String beanID = element.attributeValue("id");
             String beanClassName = element.attributeValue("class");
             BeanDefinition beanDefinition = new BeanDefinition(beanID, beanClassName);
+            // 解析动作开始
+
+            // 处理属性
+            List<Element> propertyElements = element.elements("property");
+            PropertyValues propertyValues = new PropertyValues();
+            for (Element e : propertyElements) {
+                // 获取属性
+                String name = e.attributeValue("name");
+                String type = e.attributeValue("type");
+                String value = e.attributeValue("value");
+                // 添加到属性集合中
+                propertyValues.addPropertyValue(new PropertyValue(type, name, value));
+            }
+            beanDefinition.setPropertyValues(propertyValues);
+
+            // 处理构造器参数
+            List<Element> constrElements = element.elements("constructor-arg");
+            ArgumentValues argumentValues = new ArgumentValues();
+            for (Element e : constrElements) {
+                // 获取属性
+                String name = e.attributeValue("name");
+                String type = e.attributeValue("type");
+                String value = e.attributeValue("value");
+                // 添加到构造器参数集合中
+                argumentValues.addArgumentValue(new ArgumentValue(type, name, value));
+            }
+            beanDefinition.setConstructorArgumentValues(argumentValues);
+
             simpleBeanFactory.registerBeanDefinition(beanID, beanDefinition);
         }
     }
